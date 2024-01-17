c3336d2ef46ba35ff087674abdb9f0a68f703653
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2024-01-17 15:56:40 -0300

    video 1.4 - final

--- JornadaMilhasV2/JornadaMilhas.Testes.Integracao/OfertaViagemTesteIntegração.cs
+++ JornadaMilhasV2/JornadaMilhas.Testes.Integracao/OfertaViagemTesteIntegração.cs
@@ -3,24 +3,37 @@ using Xunit.Abstractions;
 
 namespace JornadaMilhas.Testes.Integracao;
 
-public class OfertaViagemTesteIntegração
+public class OfertaViagemTesteIntegração:IDisposable
 {
     //Setup
-     private readonly JornadaMilhasContext context;
-     public OfertaViagemTesteIntegração()
-    {   
+    private readonly JornadaMilhasContext context;
+    
+    public OfertaViagemTesteIntegração()
+    {     
 
-        context = new JornadaMilhasContext();
+        context = new JornadaMilhasContext();   
     }
 
     [Fact]
     public void TestaConexaoComBaseDeDados()
     {
+
+      
         //Arrange
+
         //Act
         var resultado = context.Database.CanConnectAsync();
+
         //Assert
-        Assert.True(resultado.Result);
+        Assert.True(resultado.Result) ;
+
     }
-             
+
+    //Cleanup
+    public void Dispose()
+    {     
+
+      context.Dispose();
+    }
+        
 }
