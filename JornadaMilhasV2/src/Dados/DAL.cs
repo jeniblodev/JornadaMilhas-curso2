4d0e0aab4854494ef82fb34422b1ee9a0bb3b718
bessax <andrebessax@gmail.com>
2024-01-19 13:20:46 -0300

    #Ajustes

--- JornadaMilhasV2/src/Dados/DAL.cs
+++ JornadaMilhasV2/src/Dados/DAL.cs
@@ -69,7 +69,10 @@ public class DAL:IDisposable
             _dbContext.OfertasViagem.Remove(ofertaViagem);
             _dbContext.SaveChanges();
         }
-        throw new InvalidOperationException($"Oferta para exclusão com o ID= {oferta.Id} não encontrada.");
+        else
+        {
+            throw new InvalidOperationException($"Oferta para exclusão com o ID= {oferta.Id} não encontrada.");
+        }
     }
 
     public void Dispose()
